4024628cbdd6c06f2b93a433a3fe76010e65f1ea
TiaSetiawatiSofyan <73513993+TiaSetiawatiSofyan@users.noreply.github.com>
2020-12-03 21:01:33 +0700

    Total detik

---
+++ total detik.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int j,m,d,td;
+    cout<<"Masukan Jam :";
+    cin>>j;
+    cout<<"Masukan Menit :";
+    cin>>m;
+    cout<<"Masukan Detik :";
+    cin>>d;
+    td=(j*3600)+(m*60)+d;
+    cout<<"Total Detik : "<<td;
+    return 0;
+}
